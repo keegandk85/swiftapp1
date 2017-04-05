e3e79330c0e6aa66145eb18b4427dbf04c7d2921
Keegan King <king@eagles.ewu.edu>
2017-04-04 21:37:21 -0700

    Initial Commit

---
+++ Swift App 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App 1
+//
+//  Created by Keegan King on 4/4/17.
+//  Copyright © 2017 Keegan King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
